b6a55015300dbf6837ffc87644372349c80e5dab
Sahid Reza <sahidreza976@gmail.com>
2026-07-16 09:04:03 +0530

    Stack using ArrayList

--- src/main/java/org/example/StackClassAL.java
+++ src/main/java/org/example/StackClassAL.java
@@ -1,4 +1,48 @@
 package org.example;
 
+import java.util.ArrayList;
+import static org.example.StackClassAL.Stack.isEmpty;
+
 public class StackClassAL {
+
+     static class Stack{
+
+         static ArrayList<Integer> list = new ArrayList<>();
+         public static  boolean isEmpty(){
+             return list.size() == 0;
+         }
+
+         public static void push(int data){
+             list.add(data);
+         }
+
+         public static int pop(){
+             if(isEmpty()){
+                 return -1;
+             }
+             int top = list.get(list.size()-1);
+             list.remove(list.size()-1);
+             return top;
+         }
+
+         public static int peek(){
+             if(isEmpty()){
+                 return -1;
+             }
+             return list.get(list.size()-1);
+         }
+     }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }

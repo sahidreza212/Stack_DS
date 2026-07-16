a5a4c6915e605e79cfe31de8bd4e1b8946a8e6d4
Sahid Reza <sahidreza976@gmail.com>
2026-07-16 09:08:21 +0530

    Stack using Collection Framework

---
+++ src/main/java/org/example/StackClassCF.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Stack;
+
+public class StackClassCF {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while (!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
